d5652a0d076d8204d8eba9d2ae347c0b1ace357a
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-01-13 11:37:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,40 @@ const brad= new Person('Brad', '12-05-1996');
 
 
 //  Built in constructors
+//strings
+const name1="Jeff";
+const name2= new String('Jeff');
+console.log(name1);//get string
+console.log(name2);//get object
+console.log(typeof name1);
+console.log(typeof name2);
+
+if (name1==='Jeff'){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
+
+if (name2==='Jeff'){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
+//its not equal to the string of jeff, with a double equal it will print yes
+
+//number
+const num1=5
+const num2= new Number(5);
+
+//boolean
+const bool1=true;
+const bool2= new Boolean(true);
+
+
+//arrays
+const arr1=[1,2,3,4];
+const arr2= new Array(1,2,3,4);
+
+//reqular expression
+const re1=/\w+/;
+const re2=new RegExp('\\w+')
